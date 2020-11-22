3fa2cc030a5c47adc36f62ce818dd5636e14b7da
Saudamini Keshav <Saudamini.Keshav@gmail.com>
2020-11-21 22:09:14 -0800

    book controlle

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,5 +9,21 @@ export default {
     // Gets all books
     getBooks: function () {
         return axios.get("/api/books");
+    },
+
+    // Gets the book with the given id
+    getBook: function (id) {
+        return axios.get("/api/books/" + id);
+    },
+
+    // Saves a book to the database
+    saveBook: function (savedBooks) {
+        return axios.post("/api/books", savedBooks);
+    },
+
+    // Deletes the book with the given id
+    deleteBook: function (id) {
+        return axios.delete("/api/books/" + id);
     }
+
 } 
