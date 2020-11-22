d5ab47b12e5c0a54b46edc4a99549a2811e62f2c
Saudamini Keshav <Saudamini.Keshav@gmail.com>
2020-11-21 22:41:16 -0800

    Added nav component and updated serach result

--- src/components/SearchResult/index.js
+++ src/components/SearchResult/index.js
@@ -1,48 +1,60 @@
 import React from "react";
+import "./style.css";
+import {Row, Col} from "../Grid"
 
 const SearchResult = props => {
-    return (
+    return (props.books.length === 0) ? (
         <div className="card">
             <div className="card-body player">
-                <div id="article">
-                    {props.books.map(book => {
-                        return (
-                            <li className="list-group-item">
-                                <div className="SearchResult row" id={book.title + "Card"}>
-                                    {/* col-3 show image of the book */}
-                                    <div className="bookImage col-3">
-                                        <img src={book.image} alt={book.title} />
-                                    </div>
-                                    {/* col-9 show information of the book */}
-                                    <div className="bookInfo col-9">
-                                        <div className="row">
-                                            <h2 className="bookTitle">{book.title}</h2>
-                                        </div>
-                                        <div className="row">
-                                            <h3 className="bookAuthor">{book.author}</h3>
-                                        </div>
-                                        <div className="row">
-                                            <p className="bookDescription">{book.description}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row buttonDiv ">
-                                    <button className="saveBook btn btn-primary" id={book.id} onClick={() => book.saveBook()}>
-                                        Save Book
-                                    </button>
-                                    <a href={book.link} target="_blank">
-                                        <button className="viewBook btn btn-success">
-                                            View Book
-                                        </button>
-                                    </a>
-                                </div>
-                            </li>
-                        );
-                    })}
+                <div className="article">
+                    <h3>Search Results</h3>
                 </div>
             </div>
         </div>
-
-    )
+    ) : (
+            <div className="card">
+                <div className="card-body player">
+                    <div className="article">
+                        <h3>Search Results</h3>
+                        {props.books.map(book => {
+                            return (
+                                <li className="search-list list-group-item">
+                                    <Row className="SearchResult row" id={book.title + "Card"} key={book._id}>
+                                        {/* col-3 show image of the book */}
+                                        <Col size="2" className="bookImage">
+                                            <img src={book.image} alt={book.title} />
+                                        </Col>
+                                        <Col size="1" className="emptyCol"/>
+                                        {/* col-9 show information of the book */}
+                                        <Col size="9" className="bookInfo">
+                                            <Row>
+                                                <h3 className="bookTitle">{book.title}</h3>
+                                            </Row>
+                                            <Row>
+                                                <h4 className="bookAuthor">{book.author}</h4>
+                                            </Row>
+                                            <Row>
+                                                <p className="bookDescription">{book.description}</p>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                    <br></br>
+                                    <Row className="buttonDiv ">
+                                        <button className="saveBook btn btn-primary" id={book.id} onClick={(event) => props.handleSavedButton(event)}>
+                                            Save Book
+                                        </button>
+                                        <a href={book.link} target="_blank">
+                                            <button className="viewBook btn btn-success">
+                                                View Book
+                                        </button>
+                                        </a>
+                                    </Row>
+                                </li>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        )
 }
-export default SearchResult 
+export default SearchResult
