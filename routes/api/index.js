5c79e4c0df64da9c6ccfaa3d762d6a5195273522
Saudamini Keshav <Saudamini.Keshav@gmail.com>
2020-11-21 23:43:59 -0800

    Code update and clean up

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,4 +4,4 @@ const bookRoutes = require("./books");
 // Book routes
 router.use("/books", bookRoutes);
 
-module.exports = router; 
+module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mongoose = require("mongoose");
-const routes = require("./routes")
 
-
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
-
+// Define API routes here
 app.use(routes)
 
-//Connet to mongoose
+// Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
