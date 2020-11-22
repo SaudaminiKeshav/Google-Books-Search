90109f5a91f64660f79e0eb9893679043253f2c8
Saudamini Keshav <Saudamini.Keshav@gmail.com>
2020-11-21 22:37:23 -0800

    Added Jumbotron and grid components and code clean up

--- src/pages/SearchBooks.js
+++ src/pages/SearchBooks.js
@@ -7,28 +7,36 @@ import SearchResult from "../components/SearchResult"
 
 
 class SearchBooks extends Component {
+    //create state
     state = {
         search: "",
         books: [],
-        error: ""
+        error: "",
+        message:""
     };
 
-
+    //function to take value of what enter in the search bar
     handleInputChange = event => {
         this.setState({ search: event.target.value })
     }
 
+    //function to control the submit button of the search form 
     handleFormSubmit = event => {
         event.preventDefault();
+        // once it clicks it connects to the google book api with the search value
         API.getGoogleSearchBooks(this.state.search)
             .then(res => {
                 if (res.data.items === "error") {
                     throw new Error(res.data.items);
                 }
                 else {
+                    // store response in a array
                     let results = res.data.items
+                    //map through the array 
                     results = results.map(result => {
+                        //store each book information in a new object 
                         let book = {
+                            key: result.id,
                             id: result.id,
                             title: result.volumeInfo.title,
                             author: result.volumeInfo.authors.map(author => author),
@@ -38,12 +46,23 @@ class SearchBooks extends Component {
                         }
                         return book;
                     })
-                    this.setState({ results: results, error: "" })
+                    // reset the sate of the empty books array to the new arrays of objects with properties geting back from the response
+                    this.setState({ books: results, error: "" })
                 }
             })
             .catch(err => this.setState({ error: err.items }));
     }
 
+    handleSavedButton = event => {
+        // console.log(event)
+        event.preventDefault();
+        console.log(this.state.books)
+        let savedBooks = this.state.books.filter(book => book.id === event.target.id)
+        savedBooks = savedBooks[0];
+        API.saveBook(savedBooks)
+            .then(this.setState({message: alert("Your book is saved")}))
+            .catch(err => console.log(err))
+    }
     render() {
         return (
             <Container fluid>
@@ -62,7 +81,7 @@ class SearchBooks extends Component {
                 </Container>
                 <br></br>
                 <Container>
-                    <SearchResult books={this.state.books} />
+                    <SearchResult books={this.state.books} handleSavedButton={this.handleSavedButton} />
                 </Container>
             </Container>
         )
